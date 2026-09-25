91c87e532bd46c8a09d5dea98abde8edaeba7cc2
Gnaneswar <vagulagnaneswareddy@example.com>
2026-09-25 13:49:21 +0530

    Optimize embedding model loading

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,11 +1,23 @@
 from sentence_transformers import SentenceTransformer
 
+model = None
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def get_model():
+    global model
+
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    return model
 
 
 def create_embeddings(chunks):
+    model = get_model()
 
-    embeddings = model.encode(chunks)
+    embeddings = model.encode(
+        chunks,
+        convert_to_numpy=True
+    )
 
     return embeddings
